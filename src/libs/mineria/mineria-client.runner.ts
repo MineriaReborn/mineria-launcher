3ae0333cbec24c0cf337f5e821089d0c91d42883
ilhan.neuville <ilhan.neuville@epitech.eu>
2026-05-08 02:57:41 +0800

    Revert "fix(launcher): remove -XstartOnFirstThread (incompatible with LWJGL2)"
    This reverts commit c634363508082cf7c057545513c348e343aed77b.

--- src/libs/mineria/mineria-client.runner.ts
+++ src/libs/mineria/mineria-client.runner.ts
@@ -123,7 +123,8 @@ export class MineriaClientRunner {
     const isArm64 = os.arch() === 'arm64';
 
     return [
-      ...(isMac ? ['-Djava.awt.headless=false'] : []),
+      ...(isMac && !isArm64 ? ['-XstartOnFirstThread', '-Djava.awt.headless=false'] : []),
+      ...(isMac && isArm64 ? ['-XstartOnFirstThread', '-Djava.awt.headless=false'] : []),
       `-Xms${memory.min * 1024}M`,
       `-Xmx${memory.max * 1024}M`,
       '-XX:ReservedCodeCacheSize=512m',
